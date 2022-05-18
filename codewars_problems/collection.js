56dc04a4283eea1d2c921962912c34f456be5aea
Karkigrishmin <grishmink@gmail.com>
2022-05-18 10:39:03 +0545

    add solution

--- codewars_problems/collection.js
+++ codewars_problems/collection.js
@@ -103,3 +103,15 @@ function highAndLow(numbers) {
 
 	return `${sortedArray[sortedArray.length - 1]} ${sortedArray[0]}`;
 }
+
+//Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string,
+// the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+function longest(s1, s2) {
+	let str = s1 + s2;
+	let sortedArr = str.split("").sort();
+	let uniqueString = sortedArr
+		.filter((elem, i, arr) => arr.indexOf(elem) === i)
+		.join("");
+
+	return uniqueString;
+}
