c8efbd9ad74f7e55e0b8f8839b2bf0388557a36e
karkigrishmin <grishminkarki7@gmail.com>
2023-08-28 19:43:25 +0545

    solution(Go): codewars problem 1

---
+++ codewars_problems/go/solution-1.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+// Given 2 strings, a and b, return a string of the form short+long+short,
+// with the shorter string on the outside and the longer string on the inside.
+// The strings will not be the same length, but they may be empty ( zero length ).
+
+/*
+For example: (Input1, Input2) --> output
+
+("1", "22") --> "1221"
+("22", "1") --> "1221"
+*/
+
+func main() {
+	fmt.Println(Solution("244", "1"))
+}
+
+func Solution(a, b string) string {
+	if len(a) > len(b) {
+		return b + a + b
+	}
+
+	return a + b + a
+}
